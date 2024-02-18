357dd5d39462e9a603ab7eb349257568cd28f2e3
Juan Bermudez <juanbermudez_10@ieee.org>
2024-02-18 00:14:44 -0500

    First version of the derivatives code

---
+++ seventh Semester/propellers_rotors_and_power_train/mainDerivatives.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import easygui
+# Code created by @juanbermudezg, a student of Bachelor of Science in Aeronautical Engineering
+# at Escuela de Aviación del Ejército.
+
+def centralDiffApprox(f,x,h):
+    return (f(x+h)-f(x-h))/(2*h)
+def richardsonExtrapolation(f,x,h):
+    d=(4/3)*centralDiffApprox(f,x,h/2)-(1/3)*centralDiffApprox(f,x,h)
+    return d
+def tangent_line(f, x, h):
+    slope = richardsonExtrapolation(f, x, h)
+    def tangent(x_):
+        return slope * (x_ - x) + f(x)
+    return tangent
+def main():
+    opt = 1
+    while opt == 1:
+        try:
+            os.system('cls')
+            print("Welcome to the application made by @juanbermudezg in order to know the derivative of a function in a certain value of x.")
+            print("Here are some examples of functions to write:\n1- x+5\n2- x*cos(x-1)-sin(x)\n3- x**3+4*x**2-10")
+            functionText = input('Type your function f(x): ')
+            def f(x):
+                return eval(functionText)
+            step = float(input("Enter the step size (sampling step): "))
+            x = float(input("Enter the value of x: "))
+            x_min=x-step*100
+            x_max=x+step*100
+            x_values = np.arange(x_min, x_max, step)
+            y_values = [f(x) for x in x_values]
+            valueDerivative = richardsonExtrapolation(f,x,step)
+            tangent = tangent_line(f, x, step)
+            tangent_values = [tangent(x_) for x_ in x_values]
+            print("The value of the derivative at ",x, " is ","{:.12f}".format(valueDerivative))
+            plt.plot(x_values, y_values, label="Function f(x)")
+            plt.plot(x_values, tangent_values, label="Tangent line at x=" + str(x), linestyle='--', color='green')
+            plt.title("Graph of the function and its derivative")
+            plt.text((x_max*.75), y_values[0], 'by @juanbermudezg', fontsize = 10)
+            plt.scatter(x, round(y_values[100]), color='red', label='Point (' + str(x) + ', ' + str(round(y_values[100])) + ')')
+            plt.xlabel("x")
+            plt.ylabel("y")
+            plt.legend()
+            plt.grid(True)
+            plt.show()
+            opt=int(input('Would you like to continue?\nYes = 1\nNo = 0\n'))
+        except:
+            easygui.msgbox("Something went wrong, please try again!", title="Alert")
+            opt = 1
+    print("Thank you for using the app, see you the next time!")
+if __name__ == "__main__":
+    main()
